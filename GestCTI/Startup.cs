61d9beb1d967ff2960fed464885483c844a4c643
rodolfo <rolyrosales23@gmail.com>
2018-07-30 22:31:08 -0400

    integrando configuracion de seguridad por defecto de asp.net

---
+++ GestCTI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GestCTI.Startup))]
+namespace GestCTI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
