cdcaaff0d305e5aecedf52bb367abbc358a7f172
rodolfo <rolyrosales23@gmail.com>
2018-08-01 15:57:49 -0400

    login ok

--- GestCTI/Controllers/AdminController.cs
+++ GestCTI/Controllers/AdminController.cs
@@ -5,12 +5,14 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
 using GestCTI.Models;
+using GestCTI.Controllers.Auth;
 
 namespace GestCTI.Controllers
 {
+    [CustomAuthorize(Roles = "admin")]
     public class AdminController : Controller
     {
-        public ActionResult Admin()
+        public ActionResult Index()
         {
             string lan = Request.UserLanguages[0];
             return View();
--- GestCTI/Controllers/CompaniesController.cs
+++ GestCTI/Controllers/CompaniesController.cs
@@ -7,9 +7,11 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using GestCTI.Models;
+using GestCTI.Controllers.Auth;
 
 namespace GestCTI.Controllers
 {
+    [CustomAuthorize(Roles = "admin")]
     public class CompaniesController : Controller
     {
         private DBCTIEntities db = new DBCTIEntities();
