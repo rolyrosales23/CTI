3fd1476cc45c3cea04857403a778825cf1bbd520
rodolfo <rolyrosales23@gmail.com>
2018-08-28 18:00:22 -0400

    modificaciones administracion

--- GestCTI/Scripts/ScriptsSistema/notyTypes.js
+++ GestCTI/Scripts/ScriptsSistema/notyTypes.js
@@ -1,21 +1,13 @@
-﻿function tempNoty(message) {
-    noty({
-        text: message,
-        layout: 'topLeft',
-        type: 'success',
-        maxVisible: 20,
-        animation: {
-            open: { height: 'toggle' }, // or Animate.css class names like: 'animated bounceInLeft'
-            close: { height: 'toggle' }, // or Animate.css class names like: 'animated bounceOutLeft'
-            easing: 'swing',
-            speed: 500 // opening & closing animation speed
-        },
-        //timeout: 30000
-    });
+﻿function merge(defaults, options) {
+    if (options !== null)
+        for (var property in options)
+            defaults[property] = options[property];
+
+    return defaults;
 }
 
-function successNoty(message) {
-    noty({
+function notyBase(message, options = null) {
+    var defaults = {
         text: message,
         layout: 'topRight',
         type: 'success',
@@ -27,55 +19,42 @@ function successNoty(message) {
             speed: 500 // opening & closing animation speed
         },
         timeout: 3000
-    });
+    };
+
+    noty( merge(defaults, options) );
 }
 
-function errorNoty(message) {
-    noty({
-        text: message,
-        layout: 'topRight',
+function tempNoty(message, options = null) {
+    notyBase(message, merge({
+        layout: 'topLeft',
+        timeout: null,
+        maxVisible: 20
+    }, options));
+}
+
+function successNoty(message, options = null) {
+    notyBase(message, options);
+}
+
+function errorNoty(message, options = null) {
+    notyBase(message, merge({
         type: 'error',
-        maxVisible: 5,
-        animation: {
-            open: { height: 'toggle' }, // or Animate.css class names like: 'animated bounceInLeft'
-            close: { height: 'toggle' }, // or Animate.css class names like: 'animated bounceOutLeft'
-            easing: 'swing',
-            speed: 500 // opening & closing animation speed
-        },
-        //timeout: 2000
-    });
+        timeout: null
+    }, options));
 }
 
-function warningNoty(message) {
-    noty({
-        text: message,
-        layout: 'topRight',
+function warningNoty(message, options = null) {
+    notyBase(message, merge({
         type: 'warning',
-        maxVisible: 5,
-        animation: {
-            open: { height: 'toggle' }, // or Animate.css class names like: 'animated bounceInLeft'
-            close: { height: 'toggle' }, // or Animate.css class names like: 'animated bounceOutLeft'
-            easing: 'swing',
-            speed: 500 // opening & closing animation speed
-        },
         timeout: 4000
-    });
+    }, options));
 }
 
-function infoNoty(message) {
-    noty({
-        text: message,
-        layout: 'topRight',
-        type: 'info',
-        maxVisible: 5,
-        animation: {
-            open: { height: 'toggle' }, // or Animate.css class names like: 'animated bounceInLeft'
-            close: { height: 'toggle' }, // or Animate.css class names like: 'animated bounceOutLeft'
-            easing: 'swing',
-            speed: 500 // opening & closing animation speed
-        },
+function infoNoty(message, options = null) {
+    notyBase(message, merge({
+        type: 'information',
         timeout: 5000
-    });
+    }, options));
 }
 
 function notify(message, type = 'success') {
