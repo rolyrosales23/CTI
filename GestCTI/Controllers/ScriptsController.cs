c0474c97f7e8103cf2fdf3baccf4e9f051558af3
rodolfo <rolyrosales23@gmail.com>
2018-08-30 16:47:08 -0400

    starting with scripts

---
+++ GestCTI/Controllers/ScriptsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GestCTI.Controllers
+{
+    public class ScriptsController : Controller
+    {
+        // GET: Scripts
+        public ActionResult Display(String id, String data)
+        {
+            ViewBag.data = data;
+            return View(id, (object) data);
+        }
+    }
+}
